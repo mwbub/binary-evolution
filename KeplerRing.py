839a4ebda7c575748618ebfa3769d18cfb1b0905
Mathew Bub <mathew.bub@gmail.com>
2019-05-28 15:01:55 -0400

    Add methods to return KeplerRing parameters

--- KeplerRing.py
+++ KeplerRing.py
@@ -14,10 +14,10 @@ class KeplerRing:
 
         Parameters
         ----------
-        j : array_like
-            Initial j vector, of the form [jx, jy, jz].
         e : array_like
             Initial e vector, of the form [ex, ey, ez].
+        j : array_like
+            Initial j vector, of the form [jx, jy, jz].
         r : array_like
             Initial position and velocity of the barycentre in Galactocentric
             cylindrical coordinates, of the form [R, vR, vT, z, vz, phi].
@@ -26,14 +26,70 @@ class KeplerRing:
         a : float
             Semi-major axis of the ring.
         """
-        self.j = j
-        self.e = e
-        self.r = r
-        self.m = m
-        self.a = a
+        self._e = e
+        self._j = j
+        self._r = r
+        self._m = m
+        self._a = a
         self._t = None
         self._ej = None
-        self._orb = Orbit(vxvv=self.r)
+        self._orb = Orbit(vxvv=self._r)
+
+    def e(self):
+        """Return the time evolution of the e vector.
+
+        Returns
+        -------
+        Array of e vectors, with the same length as the time array used for
+        integration. If this KeplerRing has never been integrated, returns the
+        initial e vector instead.
+        """
+        if self._ej is None:
+            return self._e
+        return self._ej[:, :3]
+
+    def j(self):
+        """Return the time evolution of the j vector.
+
+        Returns
+        -------
+        Array of j vectors, with the same length as the time array used for
+        integration. If this KeplerRing has never been integrated, returns the
+        initial j vector instead.
+        """
+        if self._ej is None:
+            return self._j
+        return self._ej[:, 3:]
+
+    def t(self):
+        """Return the time array used to integrate this KeplerRing.
+
+        Returns
+        -------
+        Array of time steps.
+        """
+        if self._t is None:
+            raise KeplerRingError("Use KeplerRing.integrate() before using "
+                                  "KeplerRing.t()")
+        return self._t
+
+    def m(self):
+        """Return the total mass of this KeplerRing.
+
+        Returns
+        -------
+        The mass.
+        """
+        return self._m
+
+    def a(self):
+        """Return the semi-major axis of this KeplerRing.
+
+        Returns
+        -------
+        The semi-major axis.
+        """
+        return self._a
 
 
 class KeplerRingError(Exception):
