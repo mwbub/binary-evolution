Fix calculation of r_mag using cylindrical coordinates

binary_evolution/kepler_ring.py
@@ -438,7 +438,7 @@ class KeplerRing:
             The nodal precession timescale in years.
         """
         txx, tzz = self._ttensor_mean(pot, point_mass.potential())
-        r_mag = np.sum(self._r0**2)**0.5
+        r_mag = np.sum(self._r0[:2]**2)**0.5
         return 2 * (_G * point_mass.m())**0.5 / (r_mag**1.5 * np.abs(tzz - txx))
 
     def epsilon(self, pot, point_mass):

binary_evolution/point_mass.py
@@ -66,7 +66,7 @@ class PointMass:
         which varies with the orbit of kepler_ring about the Galactic centre.
         """
         if r_mag is None:
-            r_mag = np.sum(kepler_ring.r()**2)**0.5
+            r_mag = np.sum(kepler_ring.r()[:2]**2)**0.5
 
         return ((2 * kepler_ring.m()**0.5 * r_mag**3) /
                 (3 * _G**0.5 * self._m * kepler_ring.a(pc=True)**1.5))