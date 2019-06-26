Correct relativistic precession formula

binary_evolution/kepler_ring.py
@@ -678,8 +678,8 @@ class KeplerRing:
         """
         ecc = np.linalg.norm(e)
         j_cross_e = np.cross(j, e)
-        fact = 3 * (_G * self._m)**1.5 / self._a**2.5 / _c**2 / (1 - ecc**2)
-        return fact * j_cross_e
+        tau = 3 * (_G * self._m)**1.5 / self._a**2.5 / _c**2 / (1 - ecc**2)**1.5
+        return tau * j_cross_e
 
     def _error(self):
         """Sanity check to ensure that the e and j vectors are orthogonal and